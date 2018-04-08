e471140d4c50b01c4f70042f7b869186c319fe9e
Ian McMahon <ian@ianthe.engineer>
2018-04-08 12:17:11 -0400

    added clang formatting to tf2 changes

--- moveit_ros/perception/pointcloud_octomap_updater/src/pointcloud_octomap_updater.cpp
+++ moveit_ros/perception/pointcloud_octomap_updater/src/pointcloud_octomap_updater.cpp
@@ -107,8 +107,8 @@ void PointCloudOctomapUpdater::start()
   point_cloud_subscriber_ = new message_filters::Subscriber<sensor_msgs::PointCloud2>(root_nh_, point_cloud_topic_, 5);
   if (tf_listener_ && tf_buffer_ && !monitor_->getMapFrame().empty())
   {
-    point_cloud_filter_ =
-        new tf2_ros::MessageFilter<sensor_msgs::PointCloud2>(*point_cloud_subscriber_, *tf_buffer_, monitor_->getMapFrame(), 5, root_nh_);
+    point_cloud_filter_ = new tf2_ros::MessageFilter<sensor_msgs::PointCloud2>(*point_cloud_subscriber_, *tf_buffer_,
+                                                                               monitor_->getMapFrame(), 5, root_nh_);
     point_cloud_filter_->registerCallback(boost::bind(&PointCloudOctomapUpdater::cloudMsgCallback, this, _1));
     ROS_INFO("Listening to '%s' using message filter with target frame '%s'", point_cloud_topic_.c_str(),
              point_cloud_filter_->getTargetFramesString().c_str());
@@ -184,7 +184,9 @@ void PointCloudOctomapUpdater::cloudMsgCallback(const sensor_msgs::PointCloud2::
     {
       try
       {
-        tf2::fromMsg(tf_buffer_->lookupTransform(monitor_->getMapFrame(), cloud_msg->header.frame_id, cloud_msg->header.stamp), map_H_sensor);
+        tf2::fromMsg(
+            tf_buffer_->lookupTransform(monitor_->getMapFrame(), cloud_msg->header.frame_id, cloud_msg->header.stamp),
+            map_H_sensor);
       }
       catch (tf2::TransformException& ex)
       {
